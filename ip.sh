3aa741e6725d1982aadda1def97224ffc8d0b9cc
kpfries <kpfries@gmail.com>
2013-11-18 15:30:48 +0000

    Hopefully final commitment for the forseeable future

--- ip.sh
+++ ip.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+#This reroutes port 80 to port 8080 so that we don't have to type :8080 after the address in a web browser.
 sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
