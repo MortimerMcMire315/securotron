86cb9d5efccf1433bcb65a2f0cdaa3ad0618ffdb
Seth Yoder <seth.a.yoder@gmail.com>
2013-11-18 02:24:47 +0000

    Adding everything.

--- server.js
+++ server.js
@@ -12,6 +12,24 @@ function redirect404(res, err_str){
     res.end(err_str);
 }
 
+function serve_img_by_name(urlobj, res){
+    if(urlobj.pathname.split('/')[2] == null) {
+        redirect404(res);
+        return;
+    }
+    
+    var imgname = urlobj.pathname.split('/')[2];
+    fs.readFile('./img/' + imgname, function (err, contents) {
+        if(err){
+            redirect404(res);
+            return;
+        }
+        res.writeHead(200, {'Content-Type': 'image/jpg'});
+        res.end(contents, 'binary');
+    });
+    
+}
+
 /*
  * Upon receiving a /img url, will attempt to find the image at the index of the
  * ID given in the query string like so: /img?id=54
@@ -58,13 +76,89 @@ function serve_static_files(urlobj, res){
     });
 }
 
+function timesort(dumpArray){
+
+    var timestamp = dumpArray[1].slice(0,8);
+    var daySort = [];
+    var x = 0;
+
+    for(var i = 0; i < dumpArray.length; i++){
+        if(timestamp == dumpArray[i].slice(0,8)){
+            x = x + 1;
+        } else {
+            daySort.push(x);
+            timestamp = dumpArray[i].slice(0,8);
+            x = 1;
+        }
+    }
+    daySort.push(x);
+    return daySort;
+}
+
+function timeGet(img){
+    var  year = img.slice(0,4);
+    var month = img.slice(4,6);
+    var day = img.slice(6,8);
+    var date = month + "/" + day + "/" + year;
+    return date;
+}
+
+function get_today_array(day_splits, files, img_page){
+    var acm = 0;
+    for(var i = 0; i < day_splits.length; i++){
+        var old_acm = acm;
+        acm += day_splits[i];
+        if(i == img_page) {
+            return files.slice(old_acm, acm);
+        }
+    }
+    return null;
+}
+
+function build_img_div(files, img_page){
+    var day_splits = timesort(files);
+    var today_array = get_today_array(day_splits, files, img_page);
+
+    var html_str = "";
+    
+    if(today_array == null){
+        return null;
+    }
+
+    html_str += "<div><p class='date'>you are currently viewing images from</p>\
+                 <p class='real-date'>" + timeGet(today_array[0]) + "</p>.</div><br>";
+    for(var i = 0; i < today_array.length; i++){
+        html_str += "<img src='/imgbyname/" + today_array[i] + "' class='image'>\n"
+    }
+    
+    return html_str;
+}
+
+function serve_img_grid(urlobj, res){
+    var path = urlobj.pathname.split('/')[2];
+    img_page = parseInt(path);
+    
+    fs.readdir('./img', function(err, files){
+        if(err){
+            redirect404(res);
+            return;
+        }
+        html_str = build_img_div(files, img_page);
+        if(html_str == null){    
+            redirect404(res);
+        }
+        //results is an array of raw file strings.
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(html_str);
+    });
+}
+
 /*
  * Build the home page and send it to the client.
  */
-function serve_home_page(res){
+function serve_home_page(urlobj, res){
     //Using a library of asynchronous utils to quickly
     //concatenate these files without using gross nested callbacks.
-    //
     async.map(['./static/html/base.html', './static/html/home_header.html', './static/html/home_footer.html'],
         fs.readFile, function(err, results) {
             if(err){
@@ -74,10 +168,14 @@ function serve_home_page(res){
             //Read the images directory into an array of files,
             //then access the files by index.
             fs.readdir('./img', function(err2, files){
-                html_str = results[0] + results[1];
-                for(var i = 0; i < files.length; i++){
-                    html_str += "<img src='/img/" + i.toString() + "' class='image'>\n"
+                if(err2){
+                    redirect404(res);
+                    return;
                 }
+                var html_str = results[0] + results[1];
+                html_str += "<div class='img-div'>"
+                html_str += build_img_div(files, 0);
+                html_str += "</div>";
                 html_str += results[2];
                 
                 //results is an array of raw file strings.
@@ -94,17 +192,25 @@ function serve_home_page(res){
 function url_dispatch(urlobj, res){
     switch(urlobj.pathname.split('/')[1]) {
         case '':
-            serve_home_page(res);
+            serve_home_page(urlobj, res);
             break;
     
         case 'img': 
             serve_img_url(urlobj, res);
+            break;
+            
+        case 'imgbyname':
+            serve_img_by_name(urlobj, res);
             break;
 
         case 'code':
             var text = fs.readFileSync('./server.js');    
             res.writeHead(200, {'Content-Type': 'text/plain'});
             res.end(text);
+            break;
+            
+        case 'grid':
+            serve_img_grid(urlobj, res);
             break;
 
         case 'static':
@@ -127,6 +233,6 @@ function url_dispatch(urlobj, res){
 http.createServer(function (req, res) {
     urlobj = url.parse(req.url);
     url_dispatch(urlobj, res);
-}).listen(8000, '0.0.0.0');
+}).listen(8080, '0.0.0.0');
 
 console.log('Server running.');
--- static/js/local.js
+++ static/js/local.js
@@ -1,10 +1,48 @@
+var current_page = 0;
+
 $(document).ready(function() {
-   $('.forward-back-buttons').hover(function(e) {
-       e.preventDefault();
-       $(this).css('cursor', 'pointer');
-        
-   }, function(e){
-       e.preventDefault();
-       $(this).css('cursor', 'default');
-   });
+    $('.forward-back-buttons').hover(function(e) {
+        e.preventDefault();
+        $(this).css('cursor', 'pointer');
+    }, function(e){
+        e.preventDefault();
+        $(this).css('cursor', 'default');
+    });
+
+    $('#back').click(function(e) {
+        $.ajax({
+            url: '/grid/' + (current_page - 1).toString(),
+            data: {},
+            type: 'post',
+            error: function() {
+                alert('No more images.');
+            },
+            success: function() {
+                $('.img-div').fadeOut(400, function() {
+                    current_page -= 1;
+                    $(this).load('/grid/' + current_page.toString());
+                    $(this).fadeIn();
+                });
+            }
+        });
+    });
+
+    $('#forward').click(function(e) {
+        $.ajax({
+            url: '/grid/' + (current_page + 1).toString(),
+            data: {},
+            type: 'post',
+            error: function() {
+                alert('No more images.');
+            },
+            success: function(){
+                $('.img-div').fadeOut(400, function() {
+                    current_page += 1;
+                    $(this).load('/grid/' + current_page.toString());
+                    $(this).fadeIn();
+                });
+            }
+        });
+    });
+
 });
